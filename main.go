8cff8aff94c81e231e9653debd63aadbeef86a08
leolebleis <leo.lebleis@gmail.com>
2024-10-01 18:42:37 +0100

    Trying new file system

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 	"net/http"
 )
 
+// Embed the templates directory
+//
 //go:embed templates/*
 var templatesFS embed.FS
 
 func SchedulerHandler() gin.HandlerFunc {
-	fn := func(c *gin.Context) {
+	return func(c *gin.Context) {
 		schedule := pkg.CalculateSchedule()
-
 		scheduleBeautified := pkg.FormatScheduleBeautified(schedule)
 
 		c.HTML(http.StatusOK, "miaroSchedule.tmpl", gin.H{
@@ -24,24 +25,26 @@ func SchedulerHandler() gin.HandlerFunc {
 			"ScheduleNextWorkingDay": scheduleBeautified.ScheduleNextWorkingDay,
 		})
 	}
-
-	return fn
 }
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 
-	tmpl := template.Must(template.ParseFS(templatesFS, "templates/*.tmpl"))
-
+	// Parse the templates from the embedded filesystem
+	tmpl := template.Must(template.New("").ParseFS(templatesFS, "templates/*.tmpl"))
 	router.SetHTMLTemplate(tmpl)
+
+	// Define your routes
 	router.GET("/miaro", SchedulerHandler())
+
 	return router
 }
 
 func main() {
 	router := setupRouter()
-	err := router.Run(":8081")
 
+	// Start the server
+	err := router.Run(":8081")
 	if err != nil {
 		panic(err)
 	}
